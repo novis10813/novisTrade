a3390e3ba0c0db7d520210adb5520471c21bc982
novis10813 <zhizhongz895253@gmail.com>
2025-01-19 16:34:52 +0800

    make dataserver workable
    1. It still has many bugs
    2. Need to finish TODOs

--- novisTrade/dataStream/dataserver.py
+++ novisTrade/dataStream/dataserver.py
@@ -1,4 +1,6 @@
 import json
+import uuid
+import redis
 import logging
 import asyncio
 import websockets
@@ -30,9 +32,23 @@ class DataStreamServer:
         self.logger = None
         self.config = config or {}
         
-        # subscription
+        # subscription for exchange api
         self.exchange_connections: Dict[str, ExchangeWebSocket] = {}
+        
+        self.redis_consumer = redis.Redis(
+            host="localhost",
+            port=6379,
+            db=0,
+            decode_responses=True
+        )
+        self.pubsub = self.redis_consumer.pubsub()
+        
+        # subscription for client
         self.subscriptions : Dict[Tuple[str, str, str], Set] = {}
+        self.client_connections: Dict[str, websockets.WebSocketServerProtocol] = {}
+        
+        # async state tracks
+        self.is_listening = False
         
     async def start(self):
         self.server = await websockets.serve(
@@ -49,7 +65,12 @@ class DataStreamServer:
         self.logger.addHandler(handler)
         
         self.logger.info(f"Server started at `{self.host}` port {self.port}")
-        await self.server.wait_closed()
+        
+        # tasks
+        await asyncio.gather(
+            self.listen_to_redis(),
+            self.server.wait_closed()
+        )
 
     def close(self):
         """關閉服務器"""
@@ -60,6 +81,23 @@ class DataStreamServer:
     async def _handle_exchange_connection(self, websocket: websockets.WebSocketServerProtocol, path: str):
         pass
     
+    async def subscribe_to_redis(self, channel: str):
+        self.pubsub.subscribe(channel)
+        
+    async def unsubscribe_from_redis(self, channel: str):
+        self.pubsub.unsubscribe(channel)
+        
+    async def listen_to_redis(self):
+        while True:
+            message = self.pubsub.get_message()
+            if message and message['type'] == 'message':
+                data = json.loads(message['data'])
+                topic = message['channel']
+                
+                exchange, market_type, symbol, stream_type = topic.split(':')
+                await self.broadcast(exchange, symbol, stream_type, data)
+            await asyncio.sleep(0.001) #TODO: 透過 aioredis 寫成非同步的方式，不要用 sleep
+                
     def get_exchange_connection(self, exchange_name: str) -> ExchangeWebSocket:
         """獲取或創建交易所連接"""
         if exchange_name not in self.exchange_connections:
@@ -73,13 +111,25 @@ class DataStreamServer:
     async def _handle_client_connection(self, websocket: websockets.WebSocketServerProtocol, path: str = '/'):
         """處理和 client 端的連線以及訊息"""
         try:
+            client_id = str(uuid.uuid4())
+            self.client_connections[client_id] = websocket
+            
             async for message in websocket:
                 request = json.loads(message)
+                request['client_id'] = client_id
                 response = await self._handle_request(request)
                 await websocket.send(json.dumps(response))
                 
         except websockets.exceptions.ConnectionClosed:
+            if client_id in self.client_connections:
+                del self.client_connections[client_id]
             self.logger.info("Client connection closed")
+            
+            for key in list(self.subscriptions.keys()):
+                if client_id in self.subscriptions[key]:
+                    self.subscriptions[key].discard(client_id)
+                    if not self.subscriptions[key]:
+                        del self.subscriptions[key]
             
         except json.JSONDecodeError:
             await websocket.send(json.dumps({
@@ -151,11 +201,9 @@ class DataStreamServer:
                     'message': 'Missing required fields'
                 }
                 
-            #TODO symbol 需要根據交易所的格式進行處理
-            # 所以在這邊，我傳進來的格式應該是統一的格式，而不是交易所的格式
-            # 然後在每個交易所的 websocket 類裡面，再進行格式轉換
+            #TODO symbol 可能是一個 list
             exchange = request['exchange']
-            symbol = request['symbol'] # 會是一個 list，裡面會有不同的幣對
+            symbol = request['symbol']
             market_type = request.get('market_type', 'spot')
             stream_type = request.get('stream_type', 'trade')
             client_id = request['client_id']
@@ -163,14 +211,20 @@ class DataStreamServer:
             # 獲取交易所連接
             try:
                 exchange_ws = self.get_exchange_connection(exchange)
-                # TODO: 格式轉換，且 subscribe 的方法應該是統一的 (exchange_ws 的是 subscribe(self, symbol: str, stream_type: str), binance 是 subscribe(self, params: Union[str, List[str]], market_type: str = "spot", request_id: int = 1))
                 await exchange_ws.subscribe(symbol, stream_type, market_type)
+                asyncio.create_task(exchange_ws.on_messages())
+                # TODO: 這邊如果 client 有兩個以上，就會出錯
+                
             except Exception as e:
                 return {
                     'success': False,
                     'message': f"Failed to subscribe to {exchange}: {str(e)}"
                 }
-                
+            
+            # 訂閱相關的 redis channel
+            channel = f"{exchange}:{market_type}:{symbol}:{stream_type}"
+            await self.subscribe_to_redis(channel)
+            
             subscription_key = (exchange, symbol, stream_type)
             if subscription_key not in self.subscriptions:
                 self.subscriptions[subscription_key] = set()
@@ -211,3 +265,14 @@ class DataStreamServer:
     def get_subscribers(self, exchange: str, symbol: str, data_type: str) -> Set:
         key = (exchange, symbol, data_type)
         return self.subscriptions.get(key, set())
+    
+    async def broadcast(self, exchange: str, symbol: str, stream_type: str, data: Dict[str, Any]):
+        # TODO: 更有效率的 broadcast
+        subscriptions_key = (exchange, symbol, stream_type)
+        if subscriptions_key in self.subscriptions:
+            for client_id in self.subscriptions[subscriptions_key]:
+                try:
+                    if client_id in self.client_connections:
+                        await self.client_connections[client_id].send(json.dumps(data))
+                except Exception as e:
+                    self.logger.error(f"Failed to send to client {client_id}: {str(e)}")
