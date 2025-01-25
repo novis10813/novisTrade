ade1b73cffc9828ea818d889f73c9f1535224305
novis10813 <zhizhongz895253@gmail.com>
2025-01-25 14:23:34 +0800

    add kraken api

--- novisTrade/dataStream/exchanges/__init__.py
+++ novisTrade/dataStream/exchanges/__init__.py
@@ -1,4 +1,6 @@
 from .exchange_factory import ExchangeWebSocketFactory
 from .binance_ws import BinanceWebSocket
+from .kraken_ws import KrakenWebSocket
 
 ExchangeWebSocketFactory.register("binance", BinanceWebSocket)
+ExchangeWebSocketFactory.register("kraken", KrakenWebSocket)
